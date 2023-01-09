b178d096d34e7d368f51834791a8e4784ad1e99a
vatimone <veera.a.timonen@helsinki.fi>
2023-01-09 10:15:56 +0200

    Updated script

--- train_multipledatasets.sh
+++ train_multipledatasets.sh
@@ -7,17 +7,16 @@ BATCH_SIZE=${4}
 NORMALIZE_BACKGROUND=${5}
 NUM_FILTERS=${6}
 NETWORK=${7}
-WHICH_RUN=${8}
-WANTED_CHANNEL=${9}
-LEARNING_RATE=${10}
-LOSS=${11}
-PATIENCE=${12}
-QUANTILE=${13}
-PRETRAINED_WEIGHTS=${14}
+WANTED_CHANNEL=${8}
+LEARNING_RATE=${9}
+LOSS=${10}
+PATIENCE=${11}
+QUANTILE=${12}
+PRETRAINED_WEIGHTS=${13}
 
 if [ -z "$PRETRAINED_WEIGHTS" ]; 
 then
-    python model_call_inceptionunet.py ${FOLDER} ${DATAPATH} --epochs ${EPOCHS} --batch_size ${BATCH_SIZE} --normalize_background ${NORMALIZE_BACKGROUND} --numFilters ${NUM_FILTERS} --network ${NETWORK} --which_run ${WHICH_RUN} --wanted_channel ${WANTED_CHANNEL} --learning_rate ${LEARNING_RATE} --loss ${LOSS} --patience ${PATIENCE} --quantile ${QUANTILE}
+    python model_call_inceptionunet.py ${FOLDER} ${DATAPATH} --epochs ${EPOCHS} --batch_size ${BATCH_SIZE} --normalize_background ${NORMALIZE_BACKGROUND} --numFilters ${NUM_FILTERS} --network ${NETWORK} --wanted_channel ${WANTED_CHANNEL} --learning_rate ${LEARNING_RATE} --loss ${LOSS} --patience ${PATIENCE} --quantile ${QUANTILE}
 else
-    python model_call_inceptionunet.py ${FOLDER} ${DATAPATH} --epochs ${EPOCHS} --batch_size ${BATCH_SIZE} --normalize_background ${NORMALIZE_BACKGROUND} --numFilters ${NUM_FILTERS} --network ${NETWORK} --which_run ${WHICH_RUN} --wanted_channel ${WANTED_CHANNEL} --learning_rate ${LEARNING_RATE} --loss ${LOSS} --patience ${PATIENCE} --quantile ${QUANTILE} --pretrained_weights ${PRETRAINED_WEIGHTS}
+    python model_call_inceptionunet.py ${FOLDER} ${DATAPATH} --epochs ${EPOCHS} --batch_size ${BATCH_SIZE} --normalize_background ${NORMALIZE_BACKGROUND} --numFilters ${NUM_FILTERS} --network ${NETWORK} --wanted_channel ${WANTED_CHANNEL} --learning_rate ${LEARNING_RATE} --loss ${LOSS} --patience ${PATIENCE} --quantile ${QUANTILE} --pretrained_weights ${PRETRAINED_WEIGHTS}
 fi
